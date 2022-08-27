852fcd61b47ed9cc5572597a535770f2bd278efc
HazemLenin <63303495+HazemLenin@users.noreply.github.com>
2022-08-27 22:09:26 +0200

    change typography sizing and spacing

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+    // AOS initializer
+    AOS.init({
+        disable: /bot|crawler|spider|crawling/i.test(navigator.userAgent), // Disable AOS if the visitor is a web crawler
+        once: true
+    });
+
+    // Mobile navbar toggler
     let navbarBtn = document.getElementById('navbar-btn');
-    
     navbarBtn.addEventListener('click', () => {
         navbarBtn.classList.toggle('active');
     });
-    
+
+    // Reviews slick slider
     $(".reviews-carousel").slick({
         dots: false,
         infinite: true,
